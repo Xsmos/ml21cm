d1f57e35b6b7d3e414dad2a83c816b1842ff476b
Bin Xia <bxia34@gatech.edu>
2025-02-14 09:05:51 -0600

    testing on 64*64*1024

--- evaluation/plot3D.py
+++ evaluation/plot3D.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import numpy as np
 import matplotlib
 from vedo import Volume, Plotter
+
 
 # 定义自定义 colormap
 def get_eor_cmap(vmin=-150, vmax=30):
@@ -43,6 +46,8 @@ cmap = get_eor_cmap(vmin=-150, vmax=30)
 def matplotlib_to_vedo_cmap(mpl_cmap, n_colors=256):
     # 从 matplotlib colormap 中提取颜色并生成颜色样本
     colors = mpl_cmap(np.linspace(0, 1, n_colors))
+    # 将颜色转换为 (r, g, b) 格式
+    colors = [(r, g, b) for r, g, b, _ in colors]  # drop the alpha channel
     return colors
 
 # 转换 colormap
@@ -51,9 +56,21 @@ vedo_cmap = matplotlib_to_vedo_cmap(cmap)
 # 创建 Volume 对象并应用自定义 colormap
 vol = Volume(data).cmap(vedo_cmap).mode(1)  # mode(1) 使用 MIP 渲染模式
 
+#vol.alpha([
+#    [0, 1],
+#    [0.33,0.5],
+#    [0.5,0.5],
+#    [0.68,0.5],
+#    [0.83333333, 0],
+#    [0.9,0.5],
+#    [1, 1]
+#    ])  # 0 处完全透明, 0.833333 对应黑色完全透明, 1 处完全不透明
+
+#vol.mode(0)
+#vol.alpha([0, 0.5, 1])
 # 创建 Plotter 并显示
 plotter = Plotter(bg='black', size=(800, 600))  # 设置黑色背景，限制窗口大小
 plotter.renderer.SetUseDepthPeeling(True)  # 启用 Depth Peeling
-plotter.show(vol, axes=1, interactive=True)
-plotter.camera.Zoom(5)  # 放大 5 倍
+#plotter.camera.Zoom(5)  # 放大 5 倍
+plotter.show(vol, axes=0, interactive=True)
 
